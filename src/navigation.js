a4c5a39795ceef09b7c8bb691f96d0a779ed5bb1
Brayan-Alexis <brayanzxd@gmail.com>
2023-09-07 22:07:13 -0700

    Movie details initial layout

--- src/navigation.js
+++ src/navigation.js
@@ -1,30 +1,51 @@
 import { getTrendingMoviesPreview, getGenresPreview } from './main.js';
 
+searchFormBtn.addEventListener('click', (event) => {
+    // event.preventDefault();
+    // // const searchFormInput = document.querySelector("#searchForm input");
+    // const searchFormInput = document.querySelector(".search-form-input");
+    // const searchQuery = searchFormInput.value;
+    // console.log({searchQuery});
+    // location.hash = `#search=${searchQuery}`;
+    location.hash = '#search=';
+});
+arrowBack.addEventListener('click', (event) => {
+    event.preventDefault();
+    location.hash = '';
+});
+
 window.addEventListener('load', navigator, false);
 window.addEventListener('hashchange', navigator, false);
 
 function navigator() {
     console.log({location});
 
-    if (location.hash.startsWith('#trends')) {
-        trendsPage();
-    } else if (location.hash.startsWith('#search=')) {
-        searchPage();
-    } else if (location.hash.startsWith('#movie=')) {
-        movieDetailsPage();
-    } else if (location.hash.startsWith('#genre=')) {
-        genrePage();
-    } else {
-        homePage();
-    }
+    // if (location.hash.startsWith('#trends')) {
+    //     trendsPage();
+    // } else if (location.hash.startsWith('#search=')) {
+    //     searchPage();
+    // } else if (location.hash.startsWith('#movie=')) {
+    //     movieDetailsPage();
+    // } else if (location.hash.startsWith('#genre=')) {
+    //     genrePage();
+    // } else {
+    //     homePage();
+    // }
+
+    location.hash.startsWith('#trends')    ? trendsPage()       :
+    location.hash.startsWith('#search=')   ? searchPage()       :
+    location.hash.startsWith('#movie=')    ? movieDetailsPage() :
+    location.hash.startsWith('#category=') ? categoriesPage()   :
+    homePage()
 }
 
 function homePage() {
     console.log('Home page');
 
     // Header
-    headerSection.classList.remove('header-container--long');
-    headerSection.style.backgroundImage = '';
+    headerSection.classList.remove('inactive');
+    // headerSection.classList.remove('header-container--long');
+    // headerSection.style.backgroundImage = '';
     arrowBack.classList.add('inactive');
     headerTitle.classList.remove('inactive');
     // headerGenreTitle.classList.add('inactive');
@@ -33,9 +54,18 @@ function homePage() {
     // Trending movies
     trendingPreviewSection.classList.remove('inactive');
     // trendingPreviewMovieList.classList.remove('inactive');
+    // add generic section??
+    // movieDetailSection.classList.add('inactive');
 
-    getTrendingMoviesPreview();
-    getGenresPreview();
+    // Movie details
+    movieDetailSection.classList.add('inactive');
+
+    // Check if the trending movies preview is empty and if so, get the data from the API
+    const childrenCategoriesPreview = Array.from(trendingPreviewMovieList.children);
+    if (!childrenCategoriesPreview.length){
+        getTrendingMoviesPreview();
+        getGenresPreview();
+    }
 }
 
 function trendsPage() {
@@ -48,6 +78,21 @@ function searchPage() {
 
 function movieDetailsPage() {
     console.log('Movie details page');
+
+    // Header
+    // headerSection.classList.add('header-container--long');
+    // headerSection.style.backgroundImage = '';
+    headerSection.classList.add('inactive');
+    arrowBack.classList.remove('inactive');
+    // headerGenreTitle.classList.add('inactive');
+    searchForm.classList.add('inactive');
+
+    // Trending movies
+    trendingPreviewSection.classList.add('inactive');
+    // trendingPreviewMovieList.classList.add('inactive');
+    // add generic section??
+
+    movieDetailSection.classList.remove('inactive');
 }
 
 function genrePage() {
--- src/nodes.js
+++ src/nodes.js
@@ -4,17 +4,18 @@ const qs = (selector) => document.querySelector(selector); // qs = querySelector
 // Sections
 const headerSection = $("header");
 const trendingPreviewSection = $("trendingPreview");
+const movieDetailSection = qs(".movie-detail-container");
 
 // Containers & Lists
-// searchForm = $("searchForm");
-// const trendingPreviewMovieList = $(".trending-preview-movie-list);
+const searchForm = qs(".search-form");
+const trendingPreviewMovieList = qs(".trending-preview-movie-list");
 
 // Elements
-const arrowBack = qs(".header-arrow");
+const arrowBack = qs(".arrow-back-container");
 const headerTitle = qs(".header-title");
 // const headerGenreTitle = qs(".header-title--categoryView");
 
 // const searchFormInput = $("searchForm input"); //Check if this is needed
-// const searchFormBtn = $("searchBtn");
+const searchFormBtn = $('searchBtn');
 
 
